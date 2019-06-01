b9ea290b8d465fc3c093485b516043f0e6b4e83e
Amelia Galgon <agalgon@gmail.com>
2019-06-01 11:59:41 -0400

    signup flow

--- src/screens/SignupScreen.tsx
+++ src/screens/SignupScreen.tsx
@@ -1,14 +1,46 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Text, KeyboardAvoidingView, StyleSheet} from 'react-native';
+import {NavigationScreenProps} from "react-navigation";
+import User from "../store/Auth/models/user";
+import {ROUTES} from "../store/constants";
+import Input from "../components/Ui/Input";
+import ButtonDefault from "../components/Ui/ButtonDefault";
 
-class SignupScreen extends React.PureComponent {
+class SignupScreen extends React.PureComponent<NavigationScreenProps> {
+    handleLogin = () => {
+        const { navigation: { navigate } } = this.props;
+        const defaultUser: User = {
+            firstName: "Amelia",
+            lastName: "Galgon",
+            email: "agalgon@gmail.com"
+        };
+        navigate(ROUTES.HomePage, {user: defaultUser});
+    }
+
     render () {
         return (
-            <View>
-                <Text>Sign up</Text>
-            </View>
+            <KeyboardAvoidingView
+                style={styles.container}
+                behavior={"padding"}
+            >
+                <Text>Sign in</Text>
+                <Input placeholder={"First name"}/>
+                <Input placeholder={"Last name"}/>
+                <Input placeholder={"Email"}/>
+                <Input placeholder={"Password"} hidden={true}/>
+                <Input placeholder={"Confirm password"} hidden={true}/>
+                <ButtonDefault title={"Log in"} handleClick={this.handleLogin}/>
+            </KeyboardAvoidingView>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+});
 
 export default SignupScreen;
--- src/screens/WelcomeScreen.tsx
+++ src/screens/WelcomeScreen.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+// import { Google } from 'expo';
 import { StyleSheet, Text, View} from "react-native";
 import User from '../store/Auth/models/user';
 import {
@@ -6,6 +7,7 @@ import {
 } from "react-navigation";
 import ButtonDefault from '../components/Ui/ButtonDefault';
 import { ROUTES } from "../store/constants";
+// import { IOS_CLIENT_ID } from 'react-native-dotenv';
 
 interface Props {
     user: User;
@@ -25,13 +27,52 @@ const styles = StyleSheet.create({
     },
 });
 
-type FinalProps = NavigationScreenProps & Props
+type FinalProps = NavigationScreenProps & Props;
+
+/*
+async function handleClick(): Promise<void> {
+    console.log("handle google auth");
+    const clientId = '<IOS-CLIENT-ID>';
+    const result = await Google.logInAsync({
+        iosClientId: clientId,
+        scopes: ['profile', 'email'],
+    });
+
+    if (result.type === 'success') {
+        console.log(result.user);
+    }
+}
+
+*/
 
 class WelcomeScreen extends React.PureComponent<FinalProps> {
+    /*
+    constructor(props: FinalProps) {
+        super(props);
+        this.handleGoogleLogin = this.handleGoogleLogin.bind(this);
+    }
+
+    async function handleGoogleLogin(): Promise<void> {
+        console.log("handle google auth");
+        const clientId = '149545544810-e26d3cb9vbvq2a5i9593udiup4caap5f.apps.googleusercontent.com';
+        const { type, accessToken, user } = await Google.logInAsync({ iosClientId: clientId });
+
+        if (type === 'success') {
+            console.log(user);
+        }
+    }
+    */
+
+    constructor(props: FinalProps) {
+        super(props);
+    }
+
+
     render() {
         return (
             <View style={styles.container}>
                 <Text style={styles.title}>Welcome to Lost it? Found it!</Text>
+                <ButtonDefault title={"Sign in with Google"} handleClick={() => {console.log('Google Auth not implemented')}}/>
                 <ButtonDefault
                     title="Log in"
                     handleClick={() => {
